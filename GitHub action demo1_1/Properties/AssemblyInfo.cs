cceadc90828b8eba970079d4173f30549b7dc998
JustienDe <justien.decuypere@skyline.be>
2023-02-08 15:55:54 +0100

    Add project files.

---
+++ GitHub action demo1_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GitHub action demo1_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("GitHub action demo1_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("5325BB63-828D-4EE6-9864-C5AF035547AA")]
+[assembly: AssemblyVersion("1.0.0.0")]
